1bb7d67c322da3bff74373df00a7aaefd6122797
Tom5521 <mismarttv321@gmail.com>
2023-11-23 08:59:12 -0300

    Fix possible cd fail catchig

--- scripts/downloadScripts.sh
+++ scripts/downloadScripts.sh
@@ -4,12 +4,12 @@ git clone https://github.com/Tom5521/GnvimLauncher.git
 
 git clone https://github.com/Tom5521/UpgradeScript.git
 
-cd GnvimLauncher
+cd GnvimLauncher || exit
 
 go build -o ../glaunch .
 cd ..
 
-cd UpgradeScript
+cd UpgradeScript || exit
 go build -o ../upgrade .
 
 exit
