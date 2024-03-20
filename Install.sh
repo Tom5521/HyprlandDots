4fc70d6ef0ba692c85b154cac8b09ada34b3f1cb
Tom5521 <thomas.bored5521@gmail.com>
2024-03-20 16:51:27 -0300

    Improve install script

--- Install.sh
+++ Install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-cp rofi ~/.config/ -rf
-cp waypaper ~/.config/ -rf
-cp swaync ~/config/ -rf
+copy() {
+	cp -rf "$1" "$2"
+}
+
+copy rofi ~/.config/
+copy waypaper ~/.config/
+copy swaync ~/config/
+copy waybar ~/config/
+copy swaync ~/config/
 
 echo Building plugins...
 
-cd ./plugins/
+cd ./plugins/ || exit
 bash build_plugins.sh
