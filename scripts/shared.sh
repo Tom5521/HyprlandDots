8334d6c23e6e8e406a4066411b72071177a875d0
Tom5521 <thomas.bored5521@gmail.com>
2024-04-22 23:53:44 +0000

    Update dots

--- scripts/shared.sh
+++ scripts/shared.sh
@@ -3,7 +3,6 @@
 RunKitty() {
 	# I run the command with the shell to make kitty run with the user's custom environment variables.
 	$SHELL -c "kitty $3 -T '$1' --class '$1' --hold sh -c '$2'"
-	return 0
 }
 
 Toggle() {
