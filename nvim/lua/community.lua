b26a8cb7dd65f75a1a438bfdb7033be8f7efd3df
Tom5521 <thomas.bored5521@gmail.com>
2024-07-01 20:10:57 -0400

    Update nvim repo

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -6,26 +6,29 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
+	"AstroNvim/astrocommunity",
 
-  -- INFO: LAUNGUAGE PACKS
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.hyprlang" },
-  { import = "astrocommunity.pack.html-css" },
-  { import = "astrocommunity.pack.just" },
-  { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.toml" },
-  { import = "astrocommunity.pack.yaml" },
-  { import = "astrocommunity.pack.bash" },
+	-- INFO: LAUNGUAGE PACKS
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.go" },
+	{ import = "astrocommunity.pack.hyprlang" },
+	{ import = "astrocommunity.pack.html-css" },
+	{ import = "astrocommunity.pack.just" },
+	{ import = "astrocommunity.pack.json" },
+	{ import = "astrocommunity.pack.markdown" },
+	{ import = "astrocommunity.pack.python" },
+	{ import = "astrocommunity.pack.toml" },
+	{ import = "astrocommunity.pack.yaml" },
+	{ import = "astrocommunity.pack.bash" },
 
-  -- INFO: CODE RUNNERS
-  { import = "astrocommunity.code-runner.executor-nvim" },
-  { import = "astrocommunity.code-runner.compiler-nvim" },
+	-- INFO: CODE RUNNERS
+	{ import = "astrocommunity.code-runner.executor-nvim" },
+	{ import = "astrocommunity.code-runner.compiler-nvim" },
 
-  --INFO: GIT
-  { import = "astrocommunity.git.diffview-nvim" },
-  { import = "astrocommunity.git.fugit2-nvim" },
+	--INFO: GIT
+	{ import = "astrocommunity.git.diffview-nvim" },
+	{ import = "astrocommunity.git.fugit2-nvim" },
+
+	--INFO: INDENT
+	{ import = "astrocommunity.indent.indent-rainbowline" },
 }
