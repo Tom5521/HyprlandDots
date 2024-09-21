26bd9d0eef24266eab743704673b9eaa5bc3ab42
Tom5521 <thomas.bored5521@gmail.com>
2024-09-20 23:51:59 -0400

    Fix waybar toggle script

--- scripts/toggle_waybar.sh
+++ scripts/toggle_waybar.sh
@@ -4,6 +4,6 @@ cd ~/.config/hypr/scripts/
 source ./shared.sh
 
 program=waybar
-exec_cmd="waybar -c ~/.config/hypr/waybar/config.jsonc -s ~/.config/hypr/waybar/style.css"
+exec_cmd="waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css"
 
 Toggle "$program" "$exec_cmd" 1
