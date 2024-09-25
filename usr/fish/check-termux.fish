e5295f4c12855cfac14873e8fc83b1b4dbb9e394
Tom5521 <thomas.bored5521@gmail.com>
2024-09-25 18:46:20 -0400

    Add termux-friendly options

---
+++ usr/fish/check-termux.fish
@@ -0,0 +1,9 @@
+
+if set -q TERMUX_VERSION
+  set IS_TERMUX 1
+end
+
+if not set -q PREFIX
+  set PREFIX /usr
+end
+
--- usr/fish/config.fish
+++ usr/fish/config.fish
@@ -1,16 +1,20 @@
-if status is-interactive 
+if status is-interactive
     set SKIP_GREETING 0 # 1 to disable usr and root grettings
+
+    source ./check-termux.fish
 
     # Functions
     function fish_greeting
         if test $SKIP_GREETING -eq 1
             return
         end
-        set cows ~/.config/fish
+        set -l cows ~/.config/fish
         if test (whoami) = "root"
-            cowsay -e "0" -f "$cows/shark.cow" "What are we breaking today, boss?" | lolcat -a -d 1
+            cowsay -e "0" -f "$cows/shark.cow" "What are we breaking today, boss?" \
+            | lolcat -a -d 1
         else
-            cowsay -e "^" -f "$cows/fish.cow" "Welcome to fish shell, $(whoami)!" | lolcat
+            cowsay -e "^" -f "$cows/fish.cow" "Welcome to fish shell, $(whoami)!" \
+            | lolcat
         end
     end
     function get-public-ip
@@ -20,10 +24,29 @@ if status is-interactive
         telnet towel.blinkenlights.nl $argv
     end
 
+    function mv
+        command -v advmv > /dev/null
+        if test $status -eq 0
+            echo $status
+            advmv -g $argv
+        else
+            $PREFIX/bin/mv $argv
+        end
+    end
+
+    function cp
+        command -v advcp $argv
+        if test $status -eq 0
+            echo $status
+            advcp -g $argv
+        else
+            $PREFIX/bin/cp $argv
+        end
+    end
+
     # Aliases
+    function which;command -v $argv;end
     function ls;eza --icons $argv;end
-    function mv;advmv -g $argv;end
-    function cp;advcp -g $argv;end
     function cls;clear $argv;end
     function la;eza -la --icons $argv;end
     function dir;ls $argv;end
