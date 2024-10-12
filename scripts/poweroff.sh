3d64e5a00828d8455926fbd6da4e8044b0d086e6
Tom5521 <thomas.bored5521@gmail.com>
2024-10-12 00:24:32 -0300

    Add poweroff and restart popups

---
+++ scripts/poweroff.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+zenity --question --text="Do you really want to power off?" && poweroff
---
+++ scripts/reboot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+zenity --question --text="Do you really want to restart?" && reboot
