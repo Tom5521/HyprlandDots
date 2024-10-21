b6f8b5be660acfeea950c1ed7931da3ffb4c551a
Tom5521 <thomas.bored5521@gmail.com>
2024-10-21 20:32:45 -0300

    Update kernel install script

--- system/kernel/install.sh
+++ system/kernel/install.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-# Clean
-rm -rf ./linux-clear
+mkdir -p builds
 
 git clone https://aur.archlinux.org/linux-clear.git
-cd ./linux-clear || exit $?
+directory=linux-clear
 # checkout to version 6.10
-git checkout 828cd003563a82b7a2755036aab04742161333a1
+git -C $directory checkout 828cd003563a82b7a2755036aab04742161333a1
 
-patch ./PKGBUILD ../changes.patch
+patch ./linux-clear/PKGBUILD ./changes.patch
 
-time makepkg -si --noconfirm
+# Install only dependencies.
+
+echo -n "Password: "
+read -r password
+
+makepkg -D $directory -s --nobuild --noarchive --noextract --noconfirm
+
+# Build
+time makepkg -D $directory --noconfirm
+
+kernel=./linux-clear/linux-clear-6.10.12-1-x86_64.pkg.tar.zst
+headers=./linux-clear/linux-clear-headers-6.10.12-1-x86_64.pkg.tar.zst
+
+cp -rf $kernel $headers ./builds/
+
+echo "$password" | sudo -S pacman -U $kernel $headers --noconfirm
