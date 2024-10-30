540290d3e36ade53831273368067702fa1f00166
Tom5521 <thomas.bored5521@gmail.com>
2024-10-30 19:26:53 -0300

    Update kernel install script

--- system/kernel/install.sh
+++ system/kernel/install.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
-mkdir -p builds
-
-git clone https://aur.archlinux.org/linux-clear.git
-directory=linux-clear
-# checkout to version 6.10
-git -C $directory checkout 828cd003563a82b7a2755036aab04742161333a1
-
-patch ./linux-clear/PKGBUILD ./changes.patch
-
-# Install only dependencies.
-
-echo -n "Password: "
-read -r password
-
-makepkg -D $directory -s --nobuild --noarchive --noextract --noconfirm
-
-# Build
-time makepkg -D $directory --noconfirm
-
-kernel=./linux-clear/linux-clear-6.10.12-1-x86_64.pkg.tar.zst
-headers=./linux-clear/linux-clear-headers-6.10.12-1-x86_64.pkg.tar.zst
-
-cp -rf $kernel $headers ./builds/
-
-echo "$password" | sudo -S pacman -U $kernel $headers --noconfirm
+yay -S linux-xanmod-x64v2 \
+  linux-xanmod-x64v2-headers \
+  linux-firmware linux-clear-bin \
+  linux-clear-headers-bin \
+  linux-xanmod-lts-linux-bin-x64v2 \
+  linux-xanmod-lts-linux-headers-bin-x64v2
